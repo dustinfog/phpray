836fbe8b8e4981bcc31cc13ea55f1dd6cd58d659
panzhandong <xodger@163.com>
2015-03-10 16:06:07 +0800

    project list

--- classes/PHPRay/Util/Config.php
+++ classes/PHPRay/Util/Config.php
@@ -10,7 +10,25 @@ namespace PHPRay\Util;
 
 
 class Config {
+    const IGNORE_FILE_PATTERN = "/(.*sample\\.php)|(^\\..*)/";
+
     public static function load($config) {
+        $dir = PHPRAY_CONF_ROOT . $config;
+        if(is_dir($dir)) {
+            $configs = array();
+            foreach(scandir($dir) as $fileName) {
+                if(preg_match(self::IGNORE_FILE_PATTERN, $fileName)) {
+                    continue;
+                }
+
+
+                $configs[] = include_once($dir . DIRECTORY_SEPARATOR . $fileName);
+            }
+
+            return $configs;
+        }
+
+
         $file = PHPRAY_CONF_ROOT . $config . ".php";
         if(is_file($file)) {
             return include_once($file);
