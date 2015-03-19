6eacbcbc973c8110ce8b652470bca30d820111a4
panzhandong <xodger@163.com>
2015-03-19 17:35:33 +0800

    login

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,7 @@
 <?php
-
-include_once("vendor/autoload.php");
-
 define("PHPRAY_APP_ROOT", __DIR__. DIRECTORY_SEPARATOR);
 define("PHPRAY_CONF_ROOT", PHPRAY_APP_ROOT . "conf" . DIRECTORY_SEPARATOR);
+
+ini_set("error_log", PHPRAY_APP_ROOT . "logs/error.log");
+
+include_once("vendor/autoload.php");
