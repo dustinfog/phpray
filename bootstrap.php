3f912221f788c6c81e9e6d5a0e2ecac912d78068
panzhandong <xodger@163.com>
2015-04-03 14:16:52 +0800

    inherent support

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,6 @@ define("PHPRAY_APP_ROOT", __DIR__. DIRECTORY_SEPARATOR);
 define("PHPRAY_CONF_ROOT", PHPRAY_APP_ROOT . "conf" . DIRECTORY_SEPARATOR);
 
 ini_set("error_log", PHPRAY_APP_ROOT . "logs/error.log");
+ini_set('memory_limit', '-1');
 
 include_once("vendor/autoload.php");
