ff5165d241fb9099483e5988ddc91e2ba70edb84
zhandong.pan <zhandong.pan@funplus.com>
2020-03-31 18:40:34 +0800

    shutup

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,9 @@ define("PHPRAY", true);
 define("PHPRAY_APP_ROOT", __DIR__. DIRECTORY_SEPARATOR);
 define("PHPRAY_CONF_ROOT", PHPRAY_APP_ROOT . "conf" . DIRECTORY_SEPARATOR);
 
+
+ini_set("display_errors", 0);
+
 ini_set("error_log", PHPRAY_APP_ROOT . "logs/error.log");
 ini_set('memory_limit', '-1');
 
